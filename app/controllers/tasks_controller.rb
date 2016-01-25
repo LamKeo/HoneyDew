4c006276e5137b4998684bd97dfc1ec876c50f10
Lamthong Keophalychanh <thea913@gmail.com>
2016-01-25 13:34:00 -0600

    index route and view are working, creating new item working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,25 @@
 class TasksController < ApplicationController
 
-  
+  def index
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params.require(:task).permit(:title, :description))
+    if @task.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+private
+
+  def tasks_params
+    params.require(:task).permit(:title, :description)
+  end
 
 end
