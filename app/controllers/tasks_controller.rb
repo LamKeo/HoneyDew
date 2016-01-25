5770bf4ccd66c3aa3777a8ec4d07b3eea0ccd850
Lamthong Keophalychanh <thea913@gmail.com>
2016-01-25 14:33:58 -0600

    edit, update, destroy actions and views are working. Routes are a go

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,10 +6,7 @@ class TasksController < ApplicationController
   end
 
   def show
-
   end
-
-
 
   def new
     @task = Task.new
@@ -22,6 +19,22 @@ class TasksController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to root_path
   end
 
 private
