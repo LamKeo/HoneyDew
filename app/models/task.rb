630348677d8611fb02f6b6a1056d3f4fc2b64539
Lamthong Keophalychanh <thea913@gmail.com>
2016-01-26 22:41:11 -0600

    devise is working, able to assign user_id to coordinating list when logged in, tested in pry

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ActiveRecord::Base
+  # creating an association between the user and tasks
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160125205005) do
+ActiveRecord::Schema.define(version: 20160127030304) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,8 +32,8 @@ ActiveRecord::Schema.define(version: 20160125205005) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
